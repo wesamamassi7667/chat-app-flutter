c04ebdde8c5e65a92a8a6bb7be311f9b64a006b8
wesamamassi7667 <wesam199455@gmail.com>
2021-11-17 16:35:35 -0800

    splash page

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,4 +1,6 @@
+import 'package:chat_app/provider/auth_model.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 class SplashScreen extends StatefulWidget {
   const SplashScreen({Key? key}) : super(key: key);
 
@@ -7,10 +9,38 @@ class SplashScreen extends StatefulWidget {
 }
 
 class _SplashScreenState extends State<SplashScreen> {
-
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    Future.delayed(Duration(seconds: 2),()=> _checkSignedIn()
+    );
+  }
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Center(
+        child:Column(
+          children: [
+            Icon(
+                Icons.chat_rounded,
+                size: 50,
+            ),
+
+          ],
+        ),
+      ),
+    );
+  }
+
+  void _checkSignedIn() {
+   AuthModel _authModel=context.read<AuthModel>();
+   if(_authModel.isLoggedIn()){
+     Navigator.pushReplacementNamed(context, '/home');
+   }
+   else{
+     Navigator.pushReplacementNamed(context, '/login');
+   }
   }
 }
 
