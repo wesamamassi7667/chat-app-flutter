07862fc4f546d592aea2b4a1db7ccd3c7dd37830
wesamamassi7667 <wesam199455@gmail.com>
2021-11-17 16:24:09 -0800

    provider isLoggedIn method

--- lib/provider/auth_model.dart
+++ lib/provider/auth_model.dart
@@ -1,6 +1,12 @@
 
 import 'package:flutter/cupertino.dart';
-
+import 'package:google_sign_in/google_sign_in.dart';
 class AuthModel extends ChangeNotifier{
+GoogleSignInAccount? currentUser;
+
+ bool isLoggedIn(){
+ return currentUser!=null;
+ }
+
 
 }
