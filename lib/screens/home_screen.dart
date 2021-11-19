e0f62ad7b2232831313dd90d8992952c86a3954a
wesamamassi7667 <wesam199455@gmail.com>
2021-11-19 00:34:35 -0800

    add bubble package initial home ui

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,9 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:bubble/bubble.dart';
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home'),
+        centerTitle: true,
+      ),
+      body: ListView.builder(
+        itemCount: 10,
+        itemBuilder: (context,index){
+          return Bubble(
+            color: Color.fromRGBO(212, 234, 244, 1.0),
+            child: Text('TODAY', textAlign: TextAlign.center, style: TextStyle(fontSize: 11.0)),
+          );
+
+        },
+      ),
+    );
   }
 }
